a05b974b08db4e163b90acdb0270d5202cbd06a9
yiranruan <yruan1@student.unimelb.edu.au>
2019-10-17 06:19:11 +1100

    Task version 3

--- app/src/main/java/com/example/todolist/tasks/TaskActivity.java
+++ app/src/main/java/com/example/todolist/tasks/TaskActivity.java
@@ -1,67 +1,101 @@
 package com.example.todolist.tasks;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.appcompat.widget.Toolbar;
-import androidx.fragment.app.Fragment;
-import androidx.fragment.app.FragmentPagerAdapter;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.ViewPropertyAnimatorListener;
 
-import com.crashlytics.android.Crashlytics;
 import com.example.todolist.R;
-import com.github.florent37.hollyviewpager.HollyViewPager;
-import com.github.florent37.hollyviewpager.HollyViewPagerConfigurator;
 
-import butterknife.BindView;
-import butterknife.ButterKnife;
-import io.fabric.sdk.android.Fabric;
 
-public class TaskActivity extends AppCompatActivity {
-    int pageCount = 10;
-
-    @BindView(R.id.toolbar)
-    Toolbar toolbar;
-
-    @BindView(R.id.hollyViewPager)
-    HollyViewPager hollyViewPager;
+public class TaskActivity extends AppCompatActivity implements View.OnClickListener{
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        Fabric.with(this, new Crashlytics());
         setContentView(R.layout.activity_task);
-        ButterKnife.bind(this);
-        setSupportActionBar(toolbar);
-        toolbar.setTitleTextColor(0xFFFFFFFF);
-        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        initUI();
+    }
 
-        hollyViewPager.getViewPager().setPageMargin(getResources().getDimensionPixelOffset(R.dimen.viewpager_margin));
-        hollyViewPager.setConfigurator(new HollyViewPagerConfigurator() {
-            @Override
-            public float getHeightPercentForPage(int page) {
-                return ((page + 4) % 10) / 10f;
-            }
-        });
+    private void initUI() {
+        final View btnHorizontalNtb = findViewById(R.id.btn_horizontal_ntb);
+        btnHorizontalNtb.setOnClickListener(this);
+        final View btnHorizontalCoordinatorNtb = findViewById(R.id.btn_horizontal_coordinator_ntb);
+        btnHorizontalCoordinatorNtb.setOnClickListener(this);
+        final View btnTopHorizontalNtb = findViewById(R.id.btn_horizontal_top_ntb);
+        btnTopHorizontalNtb.setOnClickListener(this);
+        final View btnVerticalNtb = findViewById(R.id.btn_vertical_ntb);
+        btnVerticalNtb.setOnClickListener(this);
+        final View btnSamplesNtb = findViewById(R.id.btn_samples_ntb);
+        btnSamplesNtb.setOnClickListener(this);
+    }
 
-        hollyViewPager.setAdapter(new FragmentPagerAdapter(getSupportFragmentManager()) {
-            @Override
-            public Fragment getItem(int position) {
-                //if(position%2==0)
-                //    return new RecyclerViewFragment();
-                //else
-                return ScrollViewFragment.newInstance((String) getPageTitle(position));
-            }
+    @Override
+    public void onClick(final View v) {
+        ViewCompat.animate(v)
+                .setDuration(200)
+                .scaleX(0.9f)
+                .scaleY(0.9f)
+                .setInterpolator(new CycleInterpolator())
+                .setListener(new ViewPropertyAnimatorListener() {
+                    @Override
+                    public void onAnimationStart(final View view) {
 
-            @Override
-            public int getCount() {
-                return pageCount;
-            }
+                    }
 
-            @Override
-            public CharSequence getPageTitle(int position) {
-                return "TITLE " + position;
-            }
-        });
+                    @Override
+                    public void onAnimationEnd(final View view) {
+                        switch (v.getId()) {
+                            case R.id.btn_horizontal_ntb:
+                                startActivity(
+                                        new Intent(TaskActivity.this, HorizontalNtbActivity.class)
+                                );
+                                break;
+                            case R.id.btn_horizontal_top_ntb:
+                                startActivity(
+                                        new Intent(TaskActivity.this, TopHorizontalNtbActivity.class)
+                                );
+                                break;
+                            case R.id.btn_horizontal_coordinator_ntb:
+                                startActivity(
+                                        new Intent(TaskActivity.this, HorizontalCoordinatorNtbActivity.class)
+                                );
+                                break;
+                            case R.id.btn_vertical_ntb:
+                                startActivity(
+                                        new Intent(TaskActivity.this, VerticalNtbActivity.class)
+                                );
+                                break;
+                            case R.id.btn_samples_ntb:
+                                startActivity(
+                                        new Intent(TaskActivity.this, SamplesNtbActivity.class)
+                                );
+                                break;
+                            default:
+                                break;
+                        }
+                    }
+
+                    @Override
+                    public void onAnimationCancel(final View view) {
+
+                    }
+                })
+                .withLayer()
+                .start();
+    }
+
+    private class CycleInterpolator implements android.view.animation.Interpolator {
+
+        private final float mCycles = 0.5f;
+
+        @Override
+        public float getInterpolation(final float input) {
+            return (float) Math.sin(2.0f * mCycles * Math.PI * input);
+        }
     }
 }
 
