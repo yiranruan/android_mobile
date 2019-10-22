66a4155ceea0b4c776b82d94a8dcf5f306f72809
yiranruan <yruan1@student.unimelb.edu.au>
2019-10-22 23:48:13 +1100

    new feature handwriting

--- app/src/main/java/com/example/mobileproject/group/ShowGroupActivity.java
+++ app/src/main/java/com/example/mobileproject/group/ShowGroupActivity.java
@@ -51,13 +51,9 @@ import okhttp3.OkHttpClient;
 import okhttp3.Request;
 import okhttp3.RequestBody;
 import okhttp3.Response;
-import yalantis.com.sidemenu.interfaces.Resourceble;
-import yalantis.com.sidemenu.interfaces.ScreenShotable;
-import yalantis.com.sidemenu.model.SlideMenuItem;
-import yalantis.com.sidemenu.util.ViewAnimator;
 
 
-public class ShowGroupActivity extends AppCompatActivity implements ViewAnimator.ViewAnimatorListener {
+public class ShowGroupActivity extends AppCompatActivity {
 
     private static final long RIPPLE_DURATION = 250;
     ViewPager viewPager;
@@ -69,8 +65,6 @@ public class ShowGroupActivity extends AppCompatActivity implements ViewAnimator
     Toolbar toolbar;
     //    RelativeLayout root;
 //    View contentHamburger;
-    private List<SlideMenuItem> list = new ArrayList<>();
-    private ViewAnimator viewAnimator;
     private DrawerLayout drawerLayout;
     private ActionBarDrawerToggle drawerToggle;
     //    private int res = R.drawable.content_music;
@@ -199,14 +193,6 @@ public class ShowGroupActivity extends AppCompatActivity implements ViewAnimator
                 drawerLayout.closeDrawers();
             }
         });
-
-
-        setActionBar();
-        createMenuList();
-        viewAnimator = new ViewAnimator<>(this, list, contentFragment, drawerLayout, this);
-
-
-
 
         final View joinG = findViewById(R.id.join);
         joinG.setOnClickListener(new View.OnClickListener() {
@@ -436,72 +422,6 @@ public class ShowGroupActivity extends AppCompatActivity implements ViewAnimator
 
 
 
-
-
-
-
-
-    private void createMenuList() {
-        SlideMenuItem menuItem0 = new SlideMenuItem(ContentFragment.CLOSE, R.drawable.ic_backspace_black_24dp);
-        list.add(menuItem0);
-        SlideMenuItem menuItem = new SlideMenuItem(ContentFragment.BUILDING, R.drawable.ic_account_balance_black_24dp);
-        list.add(menuItem);
-        SlideMenuItem menuItem2 = new SlideMenuItem(ContentFragment.BOOK, R.drawable.ic_group_black_24dp);
-        list.add(menuItem2);
-//        SlideMenuItem menuItem3 = new SlideMenuItem(ContentFragment.PAINT, R.drawable.icn_3);
-//        list.add(menuItem3);
-//        SlideMenuItem menuItem4 = new SlideMenuItem(ContentFragment.CASE, R.drawable.icn_4);
-//        list.add(menuItem4);
-//        SlideMenuItem menuItem5 = new SlideMenuItem(ContentFragment.SHOP, R.drawable.icn_5);
-//        list.add(menuItem5);
-//        SlideMenuItem menuItem6 = new SlideMenuItem(ContentFragment.PARTY, R.drawable.icn_6);
-//        list.add(menuItem6);
-//        SlideMenuItem menuItem7 = new SlideMenuItem(ContentFragment.MOVIE, R.drawable.icn_7);
-//        list.add(menuItem7);
-    }
-
-
-    private void setActionBar() {
-        Toolbar toolbar = findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
-        getSupportActionBar().setHomeButtonEnabled(true);
-        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
-        drawerToggle = new ActionBarDrawerToggle(
-                this,                  /* host Activity */
-                drawerLayout,         /* DrawerLayout object */
-                toolbar,  /* nav drawer icon to replace 'Up' caret */
-                R.string.drawer_open,  /* "open drawer" description */
-                R.string.drawer_close  /* "close drawer" description */
-        ) {
-
-            /** Called when a drawer has settled in a completely closed state. */
-            public void onDrawerClosed(View view) {
-                super.onDrawerClosed(view);
-                linearLayout.removeAllViews();
-                linearLayout.invalidate();
-            }
-
-            @Override
-            public void onDrawerSlide(View drawerView, float slideOffset) {
-                super.onDrawerSlide(drawerView, slideOffset);
-                if (slideOffset > 0.6 && linearLayout.getChildCount() == 0)
-                    viewAnimator.showMenuContent();
-            }
-
-            /** Called when a drawer has settled in a completely open state. */
-            public void onDrawerOpened(View drawerView) {
-                super.onDrawerOpened(drawerView);
-            }
-        };
-        drawerLayout.addDrawerListener(drawerToggle);
-    }
-
-    @Override
-    protected void onPostCreate(Bundle savedInstanceState) {
-        super.onPostCreate(savedInstanceState);
-        drawerToggle.syncState();
-    }
-
     @Override
     public void onConfigurationChanged(Configuration newConfig) {
         super.onConfigurationChanged(newConfig);
@@ -529,46 +449,4 @@ public class ShowGroupActivity extends AppCompatActivity implements ViewAnimator
         }
     }
 
-    private ScreenShotable replaceFragment(ScreenShotable screenShotable, int topPosition) {
-        this.res = this.res == R.drawable.content_music ? R.drawable.content_films : R.drawable.content_music;
-        View view = findViewById(R.id.content_frame);
-        int finalRadius = Math.max(view.getWidth(), view.getHeight());
-        Animator animator = ViewAnimationUtils.createCircularReveal(view, 0, topPosition, 0, finalRadius);
-        animator.setInterpolator(new AccelerateInterpolator());
-        animator.setDuration(ViewAnimator.CIRCULAR_REVEAL_ANIMATION_DURATION);
-
-        findViewById(R.id.content_overlay).setBackground(new BitmapDrawable(getResources(), screenShotable.getBitmap()));
-        animator.start();
-        ContentFragment contentFragment = ContentFragment.newInstance(this.res);
-        getSupportFragmentManager().beginTransaction().replace(R.id.content_frame, contentFragment).commit();
-        return contentFragment;
-    }
-
-    @Override
-    public ScreenShotable onSwitch(Resourceble slideMenuItem, ScreenShotable screenShotable, int position) {
-        switch (slideMenuItem.getName()) {
-            case ContentFragment.CLOSE:
-                return screenShotable;
-            default:
-                return replaceFragment(screenShotable, position);
-        }
-    }
-
-    @Override
-    public void disableHomeButton() {
-        getSupportActionBar().setHomeButtonEnabled(false);
-
-    }
-
-    @Override
-    public void enableHomeButton() {
-        getSupportActionBar().setHomeButtonEnabled(true);
-        drawerLayout.closeDrawers();
-
-    }
-
-    @Override
-    public void addViewToContainer(View view) {
-        linearLayout.addView(view);
-    }
 }
